a7851c15330dc4629d604162a7f8274a6adf4a1f
Hassan K <hassan_99@live.dk>
2022-10-30 03:41:17 +0100

    Added colors for the application

--- app/src/main/java/com/dtu/s205409/ui/theme/Color.kt
+++ app/src/main/java/com/dtu/s205409/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+// main background color
+val primaryBackground = Color(0xFF51459D)
+
+// welcome button color
+val welcomeButtonColor = Color(0XFF3ED2BB)
