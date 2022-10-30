f969da0963a361f93f58795167cd82d8044ceccc
Hassan K <hassan_99@live.dk>
2022-10-30 03:42:25 +0100

    Added NavigationController

--- app/src/main/java/com/dtu/s205409/MainActivity.kt
+++ app/src/main/java/com/dtu/s205409/MainActivity.kt
@@ -3,13 +3,10 @@ package com.dtu.s205409
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dtu.s205409.controller.Navigation
 import com.dtu.s205409.ui.theme.LykkehjuletTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +14,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LykkehjuletTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface() {
+                    Navigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LykkehjuletTheme {
-        Greeting("Android")
+        Navigation()
     }
+
 }
---
+++ app/src/main/java/com/dtu/s205409/controller/NavigationController.kt
@@ -0,0 +1,27 @@
+package com.dtu.s205409.controller
+
+/**
+ * author s205409 - Hassan Kassem
+ *
+ * The code is my own. It is used in Kind aswell.
+ *
+ * Reference link: https://github.com/Stonebank/kind/blob/master/app/src/main/java/com/dtu/kd3/kind/controller/NavigationController.kt
+ *
+ */
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.dtu.s205409.views.ComposableView
+import com.dtu.s205409.views.container.ShowWelcomeView
+
+@Composable
+fun Navigation() {
+    val navigationController = rememberNavController()
+    NavHost(navController = navigationController, startDestination = ComposableView.WelcomeView.route) {
+        composable(route = ComposableView.WelcomeView.route) {
+            ShowWelcomeView(navController = navigationController)
+        }
+    }
+}
