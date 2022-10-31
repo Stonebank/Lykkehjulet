6f03ca49df946809d27cb00f90442c9fadcc64b4
Hassan K <hassan_99@live.dk>
2022-10-31 07:41:35 +0100

    Added navigation to next view

--- app/src/main/java/com/dtu/s205409/views/container/WelcomeView.kt
+++ app/src/main/java/com/dtu/s205409/views/container/WelcomeView.kt
@@ -1,9 +1,7 @@
 package com.dtu.s205409.views.container
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -22,6 +20,7 @@ import androidx.navigation.NavController
 import com.dtu.s205409.R
 import com.dtu.s205409.ui.theme.primaryBackground
 import com.dtu.s205409.ui.theme.welcomeButtonColor
+import com.dtu.s205409.views.ComposableView
 
 @Composable
 fun ShowWelcomeView(navController: NavController) {
@@ -30,8 +29,9 @@ fun ShowWelcomeView(navController: NavController) {
             .fillMaxSize()
             .background(color = primaryBackground), verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterVertically), horizontalAlignment = Alignment.CenterHorizontally) {
             Text(text = "Velkommen til ${stringResource(id = R.string.app_name)}", fontSize = 28.sp, fontWeight = FontWeight.ExtraBold, color = Color.White, textAlign = TextAlign.Center)
-            Text(text = "Siden dette er din første gang vil vi gennemgå reglerne for lykkehjulet", color = Color.White, textAlign = TextAlign.Center, fontSize = 12.sp)
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(welcomeButtonColor), shape = CircleShape) {
+            Text(text = stringResource(id = R.string.welcome_description), color = Color.White, textAlign = TextAlign.Center, fontSize = 12.sp)
+            Spacer(modifier = Modifier.padding(5.dp))
+            Button(onClick = { navController.navigate(route = ComposableView.InitialGameView.route) }, colors = ButtonDefaults.buttonColors(welcomeButtonColor), shape = CircleShape) {
                 Text("Lad os komme igang", color = Color.White)
             }
         }
