36d7b62e13af0036d07ffa8467d3fd69ed5a1a74
Hassan K <hassan_99@live.dk>
2022-11-01 13:24:33 +0100

    Updated GameView.kt

--- app/src/main/java/com/dtu/s205409/views/container/GameView.kt
+++ app/src/main/java/com/dtu/s205409/views/container/GameView.kt
@@ -5,11 +5,16 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -18,107 +23,84 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.commandiron.spin_wheel_compose.DefaultSpinWheel
 import com.commandiron.spin_wheel_compose.SpinWheelDefaults
 import com.dtu.s205409.R
 import com.dtu.s205409.ui.theme.primaryBackground
-import com.dtu.s205409.ui.theme.welcomeButtonColor
+import com.dtu.s205409.ui.theme.primaryButtonColor
+import java.util.*
+
 
 @Composable
-fun ShowGameView(name: String?) {
+fun ShowGameView(name: String?, navController: NavController) {
 
     val pointList by remember {
         mutableStateOf(listOf("Fallit", "100", "300", "500", "600", "800", "1000", "1500"))
     }
 
-    var guessing by rememberSaveable { mutableStateOf(false) }
-    
-    var lostGame by rememberSaveable { mutableStateOf(false) }
+    var points by remember { mutableStateOf(0) }
+    var lives by remember { mutableStateOf(5) }
 
-    var points by rememberSaveable { mutableStateOf(0) }
-    var lives by rememberSaveable { mutableStateOf(5) }
-    var fallit by rememberSaveable { mutableStateOf(false) }
-
-    var isSpinning by rememberSaveable { mutableStateOf(false) }
-    var spinningResult by remember { mutableStateOf("0") }
-
-    var resultDegree by rememberSaveable { mutableStateOf(0f) }
-    var index by rememberSaveable { mutableStateOf(0) }
+    var isSpinning by remember { mutableStateOf(false) }
+    var resultDegree by remember { mutableStateOf(0f) }
 
     val context = LocalContext.current
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.background(primaryBackground)) {
-            Text(text = name ?: "Fejl", fontSize = 48.sp, color = Color.White, fontWeight = FontWeight.Bold, modifier = Modifier.padding(5.dp, 5.dp))
-            Text(text = "$points points", fontSize = 20.sp, color = Color.White, fontWeight = FontWeight.SemiBold, modifier = Modifier.padding(5.dp))
-            if (fallit)
-                Text(text = "Øv.. Du ramte fallit og mistede alle dine points", color = MaterialTheme.colors.error, fontSize = 14.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(5.dp))
-        }
-        Row(modifier = Modifier.padding(top = 25.dp, bottom = 25.dp), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
-            for (heart in 1..lives) {
-                Image(painterResource(id = R.drawable.heart), "heart", modifier = Modifier.size(24.dp, 24.dp))
+        Column(modifier = Modifier.background(color = primaryBackground)) {
+            Column(modifier = Modifier
+                .clip(RoundedCornerShape(20.dp))
+                .shadow(1.dp)
+                .background(
+                    Color.White
+                )
+                .padding(horizontal = 20.dp, vertical = 20.dp)
+                .fillMaxWidth()
+                .height(100.dp)) {
+                Text(text = name?.let { capitalize(it) } ?: "Uangivet navn", color = Color.Black, fontSize = 36.sp, fontWeight = FontWeight.Bold)
+                Text("$points points", fontWeight = FontWeight.SemiBold)
+                Text("+500 points hvis du gætter rigtig!", textAlign = TextAlign.Center)
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(), horizontalArrangement = Arrangement.End) {
+                    for (heart in 0 until lives) {
+                        Image(painterResource(id = R.drawable.heart), "heart", modifier = Modifier.size(28.dp, 24.dp))
+                    }
+                }
             }
         }
         Column(verticalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterVertically), horizontalAlignment = Alignment.CenterHorizontally) {
-            if (lostGame) {
-                Text("Du mistede alle liv og tabte spillet. \uD83D\uDE22 Vil du spille igen?", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White, textAlign = TextAlign.Center)
-                Button(onClick = {
-                    guessing = false
-                    points = 0
-                    lives = 5
-                    fallit = false
-                    isSpinning = false
-                    spinningResult = ""
-                    resultDegree = 0f
-                    lostGame = false
-                }, colors = ButtonDefaults.buttonColors(welcomeButtonColor)) {
-                    Text("Spil igen", color = Color.White)
-                }
-                return@Column
+            Spacer(modifier = Modifier.padding(30.dp))
+            DefaultSpinWheel(
+                isSpinning = isSpinning,
+                onFinish = {
+                           isSpinning = false
+                },
+                animationAttr = SpinWheelDefaults.spinWheelAnimationAttr(
+                    pieCount = pointList.size,
+                    durationMillis = 4000,
+                    delayMillis = 200,
+                    rotationPerSecond = 2f,
+                    easing = FastOutSlowInEasing,
+                    startDegree = 90f
+                ),
+                colors = SpinWheelDefaults.spinWheelColors(frameColor = Color.Black, selectorColor = Color.Red),
+                dimensions = SpinWheelDefaults.spinWheelDimensions(300.dp), resultDegree = resultDegree) {
+                    pieIndex -> Text(text = pointList[pieIndex])
             }
-            if (guessing) {
-                Text("Something...")
-            } else {
-                DefaultSpinWheel(
-                    isSpinning = isSpinning,
-                    onFinish = {
-                        guessing = true
-                        isSpinning = false
-                        spinningResult = pointList[index]
-                        if (spinningResult == pointList[0]) {
-                            fallit = true
-                            points = 0
-                        }
-                    },
-                    animationAttr = SpinWheelDefaults.spinWheelAnimationAttr(
-                        pieCount = pointList.size,
-                        durationMillis = 4000,
-                        delayMillis = 200,
-                        rotationPerSecond = 2f,
-                        easing = FastOutSlowInEasing,
-                        startDegree = 90f
-                    ),
-                    colors = SpinWheelDefaults.spinWheelColors(frameColor = Color.Black, selectorColor = Color.Red),
-                    dimensions = SpinWheelDefaults.spinWheelDimensions(300.dp), resultDegree = resultDegree) {
-                        index -> Text(text = pointList[index])
-                }
-                Text(text = spinningResult, fontSize = 16.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
-                Button(
-                    onClick = {
-                        if (isSpinning) {
-                            Toast.makeText(context, "Du har allerede drejet hjulet", Toast.LENGTH_SHORT).show()
-                            return@Button
-                        }
-                        spinningResult = "Drejer hjulet.."
-                        index = pointList.indices.random()
-                        resultDegree = (0..360).random().toFloat()
-                        isSpinning = true
-                        if (fallit)
-                            fallit = false
-                    },
-                    colors = ButtonDefaults.buttonColors(if (isSpinning) Color.White.copy(alpha = 0.5f) else welcomeButtonColor)) {
-                    Text("Drej hjulet", fontWeight = FontWeight.Bold, color = Color.White)
-                }
+            Spacer(modifier = Modifier.padding(20.dp))
+            Button(
+                onClick = {
+                    if (isSpinning) {
+                        Toast.makeText(context, "Du har allerede drejet hjulet", Toast.LENGTH_SHORT).show()
+                        return@Button
+                    }
+                    resultDegree = (0..360).random().toFloat()
+                    isSpinning = true
+                }, colors = ButtonDefaults.buttonColors(if (isSpinning) Color.White.copy(alpha = 0.5f) else primaryButtonColor)) {
+                Text("Drej hjulet", color = Color.White, fontWeight = FontWeight.Bold)
             }
         }
     }
@@ -128,5 +110,10 @@ fun ShowGameView(name: String?) {
 @Preview
 @Composable
 fun Preview() {
-    ShowGameView(name = "Hassan")
+    ShowGameView(name = "Hassan", navController = rememberNavController())
+}
+
+
+fun capitalize(input: String) : String {
+    return input.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
 }
